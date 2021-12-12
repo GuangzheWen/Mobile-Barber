f8087c921f8d475d0378ee80dffd431c53d6dc6a
Wayne <jt20452@bristol.ac.uk>
2021-12-13 07:48:44 +0800

    modify sample file

---
+++ Tests/AppointmentsTests.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+import Foundation
+
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+ let urlString = "https://raw.githubusercontent.com/GuangzheWen/Mobile-Barber/main/Sample%20data/appointmentsSampleData.json"
+
+
+struct Appointment:  Codable {
+    
+    var id: String
+    var date: Date
+    
+    var customerUsername: String
+    var barberShopName: String
+    
+    var serviceType: String
+    var cost: Double
+    var orderTimeStamp: Date
+}
+
+func fetchAppointments(completion: @escaping (Result<[Appointment], Error>) -> Void) {
+    let urlComponents = URLComponents(string: urlString)!
+    
+    let task = URLSession.shared.dataTask(with: urlComponents.url!) { data, reponse, error in
+        print(1)
+        let jsonDecoder = JSONDecoder()
+        if let data = data {
+            do {
+                let appointments = try jsonDecoder.decode(Array<Appointment>.self, from: data)
+                completion(.success(appointments))
+            } catch  {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    task.resume()
+}
+
+let a =  Appointment(id: "01", date: Date().addingTimeInterval(24*60*60), customerUsername: "user 1", barberShopName: "barber 1", serviceType: "service 1", cost: 10.0, orderTimeStamp: Date())
+print(a)
+
+fetchAppointments { (result) in
+    switch result {
+    case .success(let appointments):
+        print(appointments)
+    case .failure(let error):
+        print(error)
+    }
+
+}
+PlaygroundPage.current.finishExecution()
