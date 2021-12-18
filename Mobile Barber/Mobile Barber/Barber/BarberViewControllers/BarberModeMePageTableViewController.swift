c172e4ca2ebd3c5e5e3d5c5618ed34efb4eb29f5
Wayne <jt20452@bristol.ac.uk>
2021-12-18 13:25:28 +0800

    barber mode: navigation bar, tab bar, and swich button set

---
+++ Mobile Barber/Mobile Barber/Barber/BarberViewControllers/BarberModeMePageTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  BarberModeMePageTableViewController.swift
+//  Mobile Barber
+//
+//  Created by Wayne on 2021/12/18.
+//
+
+import UIKit
+
+class BarberModeMePageTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    @IBAction func switchToCustomerMode(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Are you sure to switch to barber mode?", message: nil, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let switchAction = UIAlertAction(title: "Switch", style: .default) { action in
+            UserDefaults.standard.setValue(true, forKey: "isCustomer")
+            UserDefaults.standard.setValue(false, forKey: "isBarber")
+            
+            exit(0)
+//            DispatchQueue.main.asyncAfter(deadline: .now()) {
+//                UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
+//                         }
+            
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(switchAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
