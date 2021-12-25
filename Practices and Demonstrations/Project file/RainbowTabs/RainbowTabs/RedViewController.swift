b6909efc471b1594dcc9e6f2b08b70430b79576d
GuangzheWen <icloudofwayne@icloud.com>
2021-12-25 15:25:12 +0800

    add practice app RainbowTabs

---
+++ Practices and Demonstrations/Project file/RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Wayne on 2021/11/17.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        tabBarItem.badgeValue = "温"
+    }
+
+
+}
+
