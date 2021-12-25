89de213afe5839c3ffaaf1cfa77924f1eb9536a4
GuangzheWen <icloudofwayne@icloud.com>
2021-12-25 15:30:32 +0800

    add practice app Meal Tracker

---
+++ Practices and Demonstrations/Project file/Meal Tracker/Meal Tracker/Food.swift
@@ -0,0 +1,13 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by Wayne on 2021/12/5.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var description: String
+}
---
+++ Practices and Demonstrations/Project file/Meal Tracker/Meal Tracker/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  Meal Tracker
+//
+//  Created by Wayne on 2021/12/5.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var food: [Food]
+}
